3f97fe35469acb6ca7d2b05e08f77f82c6b02a60
Carlos Ferrer <Carlosferrerg08@gmail.com>
2024-11-08 17:53:34 -0500

    Comment/Post CRUD operations
    Added Comment and posts crud operations

---
+++ routes/postRoutes.js
@@ -0,0 +1,73 @@
+// routes/posts.js
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/Post');
+
+// Create a new post
+router.post('/', async (req, res) => {
+  const { image, area, title, description, body, createdAt, authorId } = req.body;
+  const post = new Post({
+    image,
+    area,
+    title,
+    description,
+    body,
+    createdAt,
+    authorId
+  });
+  try {
+    const savedPost = await post.save();
+    res.status(201).json(savedPost);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// Get all posts
+router.get('/', async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Get a specific post by ID
+router.get('/:id', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) return res.status(404).json({ message: 'Post not found' });
+    res.json(post);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Update a post by ID
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedPost = await Post.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    if (!updatedPost) return res.status(404).json({ message: 'Post not found' });
+    res.json(updatedPost);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// Delete a post by ID
+router.delete('/:id', async (req, res) => {
+  try {
+    const deletedPost = await Post.findByIdAndDelete(req.params.id);
+    if (!deletedPost) return res.status(404).json({ message: 'Post not found' });
+    res.json({ message: 'Post deleted' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -18,6 +18,14 @@ app.use("/api/users", require("./routes/userRoutes"));
 // Auth Routes
 app.use("/api/auth", require("./routes/authRoutes"));
 
+// Post Route
+
+app.use("/api/posts", require("./routes/postRoutes"));
+
+// Comment Route
+
+app.use("/api/comments", require("./routes/commentsRoutes"));
+
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
