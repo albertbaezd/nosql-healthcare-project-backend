848565e2001ded499bab3afb88f607b61803d4f2
Carlos Ferrer <Carlosferrerg08@gmail.com>
2024-11-10 17:40:16 -0500

    Update commentsRoutes.js
    Added get all comments

--- routes/commentsRoutes.js
+++ routes/commentsRoutes.js
@@ -84,4 +84,14 @@ router.delete('/comment/:commentId', async (req, res) => {
   }
 });
 
+// Get all comments
+router.get('/', async (req, res) => {
+  try {
+    const comments = await Comment.find();  // Fetch all comments
+    res.json(comments);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 module.exports = router;
