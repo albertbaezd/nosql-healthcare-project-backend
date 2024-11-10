4b8f179892713c65532c02ee5aaae616a77b4e67
Albert D <albertariel2013@hotmail.com>
2024-11-10 17:37:27 -0500

    adds log message to print time.

--- server.js
+++ server.js
@@ -16,6 +16,12 @@ connectDB();
 // Middleware
 app.use(morgan("dev")); // Log incoming requests (development mode)
 app.use(express.json()); // For parsing JSON
+// Printing time
+app.use((req, res, next) => {
+  const requestTime = new Date().toISOString();
+  console.log(`[${requestTime}] ${req.method} ${req.url}`);
+  next();
+});
 
 // Routes
 app.use("/api/users", require("./routes/userRoutes"));
