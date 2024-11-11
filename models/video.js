305f7eeeee43845d6d76ce8418400aa5eea2d9d2
Albert D <albertariel2013@hotmail.com>
2024-11-11 03:23:22 -0500

    updates video model, adds limit, pagination and sorting (all optional) to getAllVideos and getVideosByArea

--- models/video.js
+++ models/video.js
@@ -1,13 +1,14 @@
 // models/Video.js
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const videoSchema = new Schema({
   title: { type: String, required: true },
   description: String,
-  thumbnail: String,       // URL to the video's thumbnail image
-  area: { type: Schema.Types.ObjectId, ref: 'HealthcareArea' },  // Reference to a healthcare area
-  createdAt: { type: Date, default: Date.now }
+  thumbnail: String, // URL to the video's thumbnail image
+  area: { type: Schema.Types.ObjectId, ref: "HealthcareArea" }, // Reference to a healthcare area
+  videoId: String,
+  createdAt: { type: Date, default: Date.now },
 });
 
-module.exports = mongoose.model('Video', videoSchema);
+module.exports = mongoose.model("Video", videoSchema);
